acf288ca6de866895b28da4defb0a6430512b4d5
Lana Cristina Fabrício <65324833+lanafabricio@users.noreply.github.com>
2020-07-17 23:30:00 -0300

    Commit arquivo
    1º commit todos arquivos site

---
+++ jcl_script.js
@@ -0,0 +1,6 @@
+function pass(){
+if(document.password.nome.value=="curso" && document.password.senha.value=="hist001/2005")
+window.open('hist_africa.html','_blank')
+else
+window.open('erro.html','_self')
+}
